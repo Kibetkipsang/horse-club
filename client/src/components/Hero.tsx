13b25de2a321718871e9bb6d0d65f2753972acb0
Kibetkipsang <denniskipsang2000@gmail.com>
2025-11-11 20:21:04 +0300

    header adjustments

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -7,7 +7,7 @@ function Hero(){
             <div className="hero-section">
             <h2 className="hero-heading">INTODUCING HORSE CLUB</h2>
             <p className="hero-par">Interelation Between Horses And Riders</p>
-            <a href="" target="_blank" className="hero-btn">Learn More</a>
+            <a href="https://github.com/Kibetkipsang" target="_blank" className="hero-btn">Learn More</a>
         </div>
         </div>
         </>
